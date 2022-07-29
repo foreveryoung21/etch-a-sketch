65ee4a91870629b570d57444d81fef373af9cea9
Rodgers Kaunda <rodgerskaunda@Rodgerss-MacBook-Pro.local>
2022-07-29 19:14:17 +0100

    project completed

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
-let container = document.querySelector(".board");
+let board = document.querySelector(".board");
 let btn = document.querySelector("#btn");
+let box = document.querySelector(".box");
+let container = document.querySelector(".container");
 
-let column =container.style.gridTemplateColumns = `repeat(${16},1fr)`
-let row = container.style.gridTemplateRows = `repeat(${16},1fr)`
+
+let column =board.style.gridTemplateColumns = `repeat(${16},1fr)`
+let row = board.style.gridTemplateRows = `repeat(${16},1fr)`
 
 
 function createDiv(){
     let div = document.createElement("div");
-    div.classList.add("box1");
+    div.classList.add("box");
     return div;
 }
 
 function createGrid(size){
+    if(size>100){
+       alert("value has to be less than 100");
+       return;
+    }
     for(let i =0; i<size*size;i++){
         let createdDiv= createDiv();
-        container.appendChild(createdDiv);
+        board.appendChild(createdDiv);
     }
 }
 
 
-btn.addEventListener("click",()=>{
+
+btn.addEventListener("click",(e)=>{
+    board.innerHTML = "";
     let size = document.querySelector("#grid-size").value;
-    let column =container.style.gridTemplateColumns = `repeat(${size},1fr)`
-    let row = container.style.gridTemplateRows = `repeat(${size},1fr)`
+
+    
+    let column =board.style.gridTemplateColumns = `repeat(${size},1fr)`
+    let row = board.style.gridTemplateRows = `repeat(${size},1fr)`
 
    createGrid(size);
     
 })
 
+
+container.addEventListener("mouseover",(e)=>{
+    if(e.target.classList.contains("box")){
+        e.target.classList.add("board-color");
+    }
+    
+})
 
 
